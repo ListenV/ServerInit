058e727dcf064801f5717b7aafbdca7c6b1bb96e
sky03 <sky03.sen@outlook.com>
2024-02-25 20:17:25 +0800

    Create alias.fish

---
+++ fish/alias.fish
@@ -0,0 +1,23 @@
+# aliases config
+alias s "sudo"
+alias c "clear"
+alias .. 'cd ..'
+alias grep 'grep --color=auto'
+alias egrep 'egrep --color=auto'
+alias fgrep 'fgrep --color=auto'
+alias df 'df -h'
+alias :q exit
+alias r "ranger"
+alias ht "htop"
+alias vi "nvim"
+alias vim "nvim"
+alias dk="docker"
+alias dkse="docker search"
+alias tm="tmux"
+alias ls="lsd"
+alias cz chezmoi
+alias czdf "chezmoi diff"
+alias czap "chezmoi apply"
+alias czup "chezmoi update"
+alias czcd "chezmoi cd"
+alias sys 'sudo systemctl'

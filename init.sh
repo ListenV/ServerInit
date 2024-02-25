3169414d85d6974b258b035479972c0f3538bcd8
sky03 <sky03.sen@outlook.com>
2024-02-25 21:03:42 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,17 @@
 set -e
 
 REPO=https://raw.githubusercontent.com/ListenV/ServerInit/main
+USER_HOME=/root
 
-wget -P /root/.config/tmux ${REPO}/tmux/tmux.conf
-wget -P /root/.config/tmux ${REPO}/tmux/powerline.conf
+wget -P ${USER_HOME}/.config/tmux ${REPO}/tmux/tmux.conf
+wget -P ${USER_HOME}/.config/tmux ${REPO}/tmux/powerline.conf
 
-wget -P /root/.config/nvim ${REPO}/nvim/init.lua
-wget -P /root/.config/nvim/lua ${REPO}/nvim/lua/basic.lua
-wget -P /root/.config/nvim/lua ${REPO}/nvim/lua/keymap.lua
-wget -P /root/.config/nvim/lua ${REPO}/nvim/lua/autocmd.lua
+wget -P ${USER_HOME}/.config/nvim ${REPO}/nvim/init.lua
+wget -P ${USER_HOME}/.config/nvim/lua ${REPO}/nvim/lua/basic.lua
+wget -P ${USER_HOME}/.config/nvim/lua ${REPO}/nvim/lua/keymap.lua
+wget -P ${USER_HOME}/.config/nvim/lua ${REPO}/nvim/lua/autocmd.lua
+
+mkdir -p ${USER_HOME}/.config/fish/conf.d
+wget -O ${USER_HOME}/.config/fish/conf.d/alias.fish ${REPO}/shell/alias.sh
+
+(curl -fsSL ${REPO}/shell/alias.sh) >> ${USER_HOME}/.bashrc

2298c94eee05cdcf203877a9d5e2e3570eff6412
sky03 <sky03.sen@outlook.com>
2024-02-25 18:48:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
-REPO=https://github.com/ListenV/ServerInit
+REPO=https://raw.githubusercontent.com/ListenV/ServerInit/main
 
+wget -P /root/.config/tmux ${REPO}/tmux/tmux.conf
+wget -P /root/.config/tmux ${REPO}/tmux/powerline.conf
 
-wget -O /root/.config/tmux/tmux.conf https://github.com/ListenV/ServerInit
+wget -P /root/.config/nvim ${REPO}/nvim/init.lua

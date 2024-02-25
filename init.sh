c72c6f5660653d918d80ddf940e85398fe4b0c2e
sky03 <sky03.sen@outlook.com>
2024-02-25 19:02:45 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ wget -P /root/.config/tmux ${REPO}/tmux/tmux.conf
 wget -P /root/.config/tmux ${REPO}/tmux/powerline.conf
 
 wget -P /root/.config/nvim ${REPO}/nvim/init.lua
+wget -P /root/.config/nvim/core ${REPO}/nvim/core/init.lua
+wget -P /root/.config/nvim/core ${REPO}/nvim/core/basic.lua
+wget -P /root/.config/nvim/core ${REPO}/nvim/core/keymap.lua
+wget -P /root/.config/nvim/core ${REPO}/nvim/core/autocmd.lua

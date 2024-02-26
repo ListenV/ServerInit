60672d710ff3a5e496084ed99a025fc77ea84766
sky03 <sky03.sen@outlook.com>
2024-02-26 09:15:40 +0800

    Create init-panel.sh

---
+++ init-panel.sh
@@ -0,0 +1,24 @@
+#!
+
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+apt update
+
+apt install php8.3 php8.3-{bcmath,bz2,cli,common,curl,fpm,gd,igbinary,mbstring,mysql,opcache,readline,redis,xml,yaml,zip} -y
+
+
+
+## 部署SSPanel
+git clone --depth 1 -b 2023.7 https://github.com/Anankke/SSPanel-Uim.git /srv/sspanel
+# 改权限
+chmod 755 /srv/sspanel -R
+chown www-data /srv/sspanel -R
+
+cd /srv/sspanel
+wget https://getcomposer.org/installer -O composer.phar
+php composer.phar
+php composer.phar install --no-dev
+
+# 改配置
+cp config/.config.example.php config/.config.php
+cp config/appprofile.example.php config/appprofile.php

0e30950bac19365dc48a97ad1246d4f22e999191
sky03 <sky03.sen@outlook.com>
2024-02-27 14:32:38 +0800

    Update init-panel.sh

--- init-panel.sh
+++ init-panel.sh
@@ -1,12 +1,12 @@
 #!
 
+# 安装PHP源
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
 apt update
 
+# 安装PHP8.3
 apt install php8.3 php8.3-{bcmath,bz2,cli,common,curl,fpm,gd,igbinary,mbstring,mysql,opcache,readline,redis,xml,yaml,zip} -y
-
-
 
 ## 部署SSPanel
 git clone --depth 1 -b 2023.7 https://github.com/Anankke/SSPanel-Uim.git /srv/sspanel

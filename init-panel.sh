c6a97865030913abb0d0df4ee77b660bf3f9e3b0
sky03 <sky03.sen@outlook.com>
2024-02-27 15:10:47 +0800

    Update init-panel.sh

--- init-panel.sh
+++ init-panel.sh
@@ -1,5 +1,10 @@
 #!
 
+# 配置 Nginx
+ufw allow http
+ufw allow https
+rm /etc/nginx/sites-enabled/default
+
 # 安装PHP源
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
@@ -9,16 +14,14 @@ apt update
 apt install php8.3 php8.3-{bcmath,bz2,cli,common,curl,fpm,gd,igbinary,mbstring,mysql,opcache,readline,redis,xml,yaml,zip} -y
 
 ## 部署SSPanel
-git clone --depth 1 -b 2023.7 https://github.com/Anankke/SSPanel-Uim.git /srv/sspanel
+git clone --depth 1 https://github.com/Anankke/SSPanel-Uim.git /srv/sspanel
 # 改权限
-chmod 755 /srv/sspanel -R
 chown www-data /srv/sspanel -R
+chmod o-rwx /srv/sspanel -R
+chmod 750 /srv/sspanel/update.sh /srv/sspanel/xcat
 
+# 安装依赖
 cd /srv/sspanel
-wget https://getcomposer.org/installer -O composer.phar
-sudo -u www-data php composer.phar
-sudo -u www-data php composer.phar install --no-dev
-
-# 改配置
-cp config/.config.example.php config/.config.php
-cp config/appprofile.example.php config/appprofile.php
+wget https://getcomposer.org/installer -O /srv/sspanel/composer.phar
+php composer.phar
+sudo -u www-data /usr/bin/php composer.phar install --no-dev

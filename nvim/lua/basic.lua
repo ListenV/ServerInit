4e741d082d1eddd74723901fe8cb29d07cc7678c
sky03 <sky03.sen@outlook.com>
2025-01-19 21:54:26 +0800

    Update basic.lua

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -1,9 +1,8 @@
 local opt = vim.opt
-local g = vim.g
 
 ------------------------------- 基础配置 ------------------------------------------
 
-g.encoding = "UTF-8"     -- utf8编码
+vim.g.encoding = "UTF-8"     -- utf8编码
 opt.fileencoding = "utf-8"
 
 opt.termguicolors = true -- 开启GUI颜色支持
@@ -13,7 +12,6 @@ opt.confirm = true       -- 直接退出时，确认保存
 opt.backup = false       -- 覆盖文件时, 禁止创建备份文件
 opt.writebackup = false  -- 如果一个文件正在被另一个程序编辑(或在另一个程序编辑时写入文件), 则不允许编辑
 opt.swapfile = false     -- 缓冲区是否使用交换文件
-opt.undofile = true      -- 把撤销信息写入一个文件里 (这样可以永久保存撤销信息)
 opt.undolevels = 1000    -- 保存撤销记录数
 
 opt.autoread = true      -- 当文件被外部程序修改时，自动加载
@@ -55,8 +53,6 @@ opt.numberwidth = 2        -- 行号宽度: 2
 opt.relativenumber = false  -- 使用相对行号
 opt.colorcolumn = "80"     -- 高亮第80列 (超过表示代码太长了，考虑换行)
 
-opt.signcolumn = "yes"   -- 显示左侧图标指示列
-
 opt.pumblend = 10 -- Popup blend
 opt.pumheight = 10 -- Maximum number of entries in a popup
 
@@ -82,18 +78,9 @@ opt.incsearch = true   -- 边输入边搜索
 
 ------------------------------------ 状态栏设置 ------------------------------------
 
-opt.showmode = false       -- 不显示 vim 当前模式 (有状态栏插件)
-opt.showtabline = 2        -- 显示顶部 tabline
-opt.cmdheight = 2          -- 命令行高为2，提供足够的显示空间
-opt.ruler = false        -- 显示光标的位置（位于哪一行哪一列）(默认开启)
 opt.wildmenu = true        -- 命令补全增强
 opt.shortmess:append('c')  -- 将显示的一些信息变为缩写，可使用`:h shortmess`查看详细信息
 opt.pumheight = 10         -- 补全最多显示10行
-
-opt.completeopt = "menu,menuone,noselect,noinsert"  -- 自动补全不自动选中
-
--- NeoVim 0.7 新特性, 控制何时显示状态栏（0不显示，1至少有两个窗口才显示，2总是显示，3总是在最后一个窗口显示）
-opt.laststatus = 3
 
 ------------------------------------- 其他设置 ------------------------------------
 
